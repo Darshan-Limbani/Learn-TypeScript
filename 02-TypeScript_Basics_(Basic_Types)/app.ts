1c151e51b3b628a647300ef984ae4fd872d38e97
Darshan Limbani <darshanlimbani94@gmail.com>
2023-03-29 16:03:49 +0530

    Working with Tuples

--- 02-TypeScript_Basics_(Basic_Types)/app.ts
+++ 02-TypeScript_Basics_(Basic_Types)/app.ts
@@ -1,10 +1,27 @@
-const person = {
+const person: {
+    name: string,
+    age: number,
+    hobbies: string[],
+    role: [number, string]
+} = {
     name: 'darshan',
     age: 21,
-    hobbies: ['Sports', 'Swimming','Drawing']
+    hobbies: ['Sports', 'Swimming', 'Drawing'],
+    role: [1, 'author']
 }
 
 console.log(person.name)
+
+let favouriteActivity: string[];
+
+
+// person.role = [2,'admin','author']  //Not Allowed
+
+// person.role.push(20)  // Allowed
+// person.role.push("Admin") // Allowed
+console.log(person.role)
+favouriteActivity = ['Sports']
+
 
 for (const hobby of person.hobbies) {
     console.log(hobby)
