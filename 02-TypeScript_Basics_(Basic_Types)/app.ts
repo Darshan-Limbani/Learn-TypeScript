9aa83277b456c2f55a14fb0dbe9a47b55f25f26a
Darshan Limbani <darshanlimbani94@gmail.com>
2023-03-29 18:11:50 +0530

    The never Type

--- 02-TypeScript_Basics_(Basic_Types)/app.ts
+++ 02-TypeScript_Basics_(Basic_Types)/app.ts
@@ -9,3 +9,9 @@ console.log(userName)
 // userName = userInput; // Not Allowed
 if (typeof userInput === 'string')
     userName = userInput;
+
+function generateError(message: string, code: number) {
+    throw {message: message, errorCode: code}
+}
+
+generateError('something went wrong!', 404)
