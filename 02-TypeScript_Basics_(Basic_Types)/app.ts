d1c3f2699bf037c2b4a2fa0e0ae1a46b996295a4
Darshan Limbani <darshanlimbani94@gmail.com>
2023-03-29 13:59:30 +0530

    Using Type

--- 02-TypeScript_Basics_(Basic_Types)/app.ts
+++ 02-TypeScript_Basics_(Basic_Types)/app.ts
@@ -1,1 +1,9 @@
-console.log('Time to get started...');
+function add(n1: number, n2: number) {
+    return n1 + n2;
+}
+
+const num1 = 10;
+const num2 = 20;
+
+const result = add(num1, num2);
+console.log(result)
