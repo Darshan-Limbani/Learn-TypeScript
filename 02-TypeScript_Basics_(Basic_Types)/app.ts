f2f66132429a3b5f4bdcc94ab7869e7be625ef3b
Darshan Limbani <darshanlimbani94@gmail.com>
2023-03-29 17:44:32 +0530

    Function as Type

--- 02-TypeScript_Basics_(Basic_Types)/app.ts
+++ 02-TypeScript_Basics_(Basic_Types)/app.ts
@@ -2,8 +2,18 @@ function add(n1: number, n2: number) {
     return n1 + n2;
 }
 
-function printResult(num: number): void {
+function printResult(num: number) {
     console.log('Result : ' + num)
 }
 
 printResult(add(10, 15))
+
+// let combinedValues: Function;
+let combinedValues: (a: number, b: number) => number;
+
+combinedValues = add
+printResult(combinedValues(5, 5))
+
+// combinedValues = printResult // Not Valid
+
+printResult(combinedValues(5, 5))
