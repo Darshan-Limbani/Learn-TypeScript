855895ccd2d4d576070df308716c3ba7ff16a089
Darshan Limbani <darshanlimbani94@gmail.com>
2023-03-29 14:21:00 +0530

    Working with Numbers, Strings and Booleans

--- 02-TypeScript_Basics_(Basic_Types)/app.ts
+++ 02-TypeScript_Basics_(Basic_Types)/app.ts
@@ -1,9 +1,15 @@
-function add(n1: number, n2: number) {
-    return n1 + n2;
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+
+    if (showResult) {
+        console.log(phrase, n1 + n2)
+    } else {
+        return n1 + n2;
+    }
 }
 
 const num1 = 10;
 const num2 = 20;
+const printResult = true;
+const resultPhrase = 'Result is :'
 
-const result = add(num1, num2);
-console.log(result)
+add(num1, num2, printResult, resultPhrase);
