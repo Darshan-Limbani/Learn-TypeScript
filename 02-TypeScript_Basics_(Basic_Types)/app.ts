639d1ef6a6d4f692c46da03abc3bc51b670d019b
Darshan Limbani <darshanlimbani94@gmail.com>
2023-03-29 17:52:08 +0530

    Function Types Callbacks

--- 02-TypeScript_Basics_(Basic_Types)/app.ts
+++ 02-TypeScript_Basics_(Basic_Types)/app.ts
@@ -6,6 +6,19 @@ function printResult(num: number) {
     console.log('Result : ' + num)
 }
 
+
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+
+    const result = n1 + n2;
+    cb(result)
+
+}
+
+addAndHandle(10, 15, (result) => {
+    console.log("Callback Fn Result : ", result)
+})
+
+
 printResult(add(10, 15))
 
 // let combinedValues: Function;
