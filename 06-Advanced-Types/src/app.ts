0efb2c4d0c1e1df42e63cfea9bb2076898e4a1a7
Darshan Limbani <darshanlimbani94@gmail.com>
2023-03-30 19:09:29 +0530

    Type Guards

--- 06-Advanced-Types/src/app.ts
+++ 06-Advanced-Types/src/app.ts
@@ -21,6 +21,56 @@ type Combinable = string | number;
 type Numeric = number | boolean;
 type Universal = Combinable & Numeric;
 
-// let demo :Universal
-// demo = 10
-// console.log(typeof demo)
+function add(a: Combinable, b: Combinable) {
+    if (typeof a === 'string' || typeof b === 'string') {   //Condition is Called type Guard
+        return a.toString() + b.toString;
+    }
+    return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+    console.log('Name : ' + emp.name);
+    if ('privileges' in emp)
+        console.log('privileges : ' + emp.privileges);
+
+    if ('startDate' in emp) {
+        console.log('Start Date : ' + emp.startDate)
+    }
+}
+
+printEmployeeInformation(e1)
+
+
+class Car {
+    drive() {
+        console.log('Driving...')
+    }
+}
+
+class Truck {
+    drive() {
+        console.log('Driving Truck...')
+    }
+
+    loadCargo(amount: number) {
+        console.log('Loading Cargo... : ' + amount)
+    }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+    vehicle.drive();
+
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(100)
+    }
+}
+
+useVehicle(v1)
+useVehicle(v2)
