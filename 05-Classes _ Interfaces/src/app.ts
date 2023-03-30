ff60f6cd47998a2f8b30ba2d15dc55b791cb9282
Darshan Limbani <darshanlimbani94@gmail.com>
2023-03-30 14:30:29 +0530

    Private and Public Access Modifier

--- 05-Classes _ Interfaces/src/app.ts
+++ 05-Classes _ Interfaces/src/app.ts
@@ -1,6 +1,8 @@
 class Department {
     name: string;
 
+    private employee: string[] = []
+
     constructor(n: string) {
         this.name = n
     }
@@ -8,10 +10,24 @@ class Department {
     describe(this: Department) {
         console.log("Department : ", this.name)
     }
+
+    addEmployee(employee: string) {
+        this.employee.push(employee)
+    }
+
+    printEmployeeInformation() {
+        console.log(this.employee.length)
+        console.log(this.employee)
+    }
 }
 
 const accounting = new Department('Accounting')
 accounting.describe()
 
-const accountingCopy = {describe: accounting.describe, name: "Marketing"}
-accountingCopy.describe()
+accounting.addEmployee('Max')
+accounting.addEmployee('John')
+// accounting.employee[2]='Anna'
+
+accounting.printEmployeeInformation()
+// const accountingCopy = {describe: accounting.describe, name: "Marketing"}
+// accountingCopy.describe()
