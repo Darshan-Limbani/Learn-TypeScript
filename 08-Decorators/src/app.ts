e125548eaf66ba0e02410f31ce6febda542c7c7e
Darshan Limbani <darshanlimbani94@gmail.com>
2023-03-31 16:26:03 +0530

    Building More Useful Decorator

--- 08-Decorators/src/app.ts
+++ 08-Decorators/src/app.ts
@@ -1,9 +1,23 @@
-function Logger(constructor: Function) {
-    console.log('Logging...')
-    console.log(constructor)
+function Logger(logString: string) {
+    return function (constructor: Function) {
+        console.log(logString)
+        console.log(constructor)
+    }
 }
 
-@Logger
+
+function withTemplate(template: string, hookId: string) {
+    return function (constructor: any) {
+        const hookEl = document.getElementById(hookId);
+        const p = new constructor();
+        if (hookEl) {
+            hookEl.innerHTML = template;
+            hookEl.querySelector('h1')!.textContent = p.name
+        }
+    }
+}
+
+@withTemplate('<h1> My Person Object</h1>', 'app')
 class Person {
     name = 'Max'
 
