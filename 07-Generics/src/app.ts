40a6c27ca8d96a132521a616b0ffe5949ea4019a
Darshan Limbani <darshanlimbani94@gmail.com>
2023-03-31 13:53:06 +0530

    Generic Classes

--- 07-Generics/src/app.ts
+++ 07-Generics/src/app.ts
@@ -47,3 +47,51 @@ function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) 
 
 console.log(extractAndConvert({name: 'Max'}, 'name'))
 
+//Generic Classes
+
+class DataStorage<T extends string|number|boolean> {
+    private data: T[] = [];
+
+    addItem(item: T) {
+        this.data.push(item)
+    }
+
+    removeItem(item: T) {
+        // if (this.data.indexOf(item) === -1) {
+        //     return;
+        // }
+        this.data.splice(this.data.indexOf(item), 1)
+    }
+
+    getItems() {
+        return [...this.data]
+    }
+
+}
+
+const textStorage = new DataStorage<string>()
+textStorage.addItem("Max")
+textStorage.addItem("Manu")
+textStorage.removeItem("Manu")
+console.log(textStorage.getItems())
+
+
+
+const numberStorage = new DataStorage<number>()
+numberStorage.addItem(10)
+numberStorage.addItem(20)
+console.log(numberStorage.getItems())
+numberStorage.removeItem(20)
+console.log(numberStorage.getItems())
+
+
+// const objStorage = new DataStorage<object>()
+// const obj1 = {name:'Max'};
+// objStorage.addItem(obj1)
+// const obj2 = {name:'Manu'};
+// objStorage.addItem(obj2)
+// console.log(objStorage.getItems())
+// objStorage.removeItem(obj2)
+// console.log(objStorage.getItems())
+
+
