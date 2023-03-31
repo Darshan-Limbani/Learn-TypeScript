b0e65529a14532de30d080cf7c91b5da9cdc3f99
Darshan Limbani <darshanlimbani94@gmail.com>
2023-03-31 17:14:53 +0530

    Property Decorators

--- 08-Decorators/src/app.ts
+++ 08-Decorators/src/app.ts
@@ -35,3 +35,30 @@ class Person {
 const person = new Person()
 
 console.log(person)
+
+function Log(target: any, propertyName: string | Symbol) {
+    console.log('Property decorator!');
+    console.log(target, propertyName);
+}
+
+class Product {
+    @Log
+    title: string;
+    _price: number;
+
+    set price(val: number) {
+        if (val > 0) {
+            this._price = val;
+        }
+        throw new Error('Invalid price - should be positive!')
+    }
+
+    constructor(t: string, p: number) {
+        this.title = t
+        this._price = p
+    }
+
+    getPriceWithText(tax: number) {
+        return this._price * (1 + tax)
+    }
+}
