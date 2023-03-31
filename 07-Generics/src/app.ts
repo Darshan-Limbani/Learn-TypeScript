e7e646de20e25de4fe1b1bf10e5ae09f502de431
Darshan Limbani <darshanlimbani94@gmail.com>
2023-03-31 13:36:35 +0530

    The keyof Constraints

--- 07-Generics/src/app.ts
+++ 07-Generics/src/app.ts
@@ -20,3 +20,30 @@ const mergedObj = merge({name: 'Max', hobbies: ['Sports']}, {age: 30})
 console.log(mergedObj)
 
 
+interface Lengthy {
+    length: number;
+}
+
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+    let des = 'Got no Value.'
+    if (element.length === 1) {
+        des = 'Got 1 element.'
+    } else if (element.length > 1) {
+        des = 'Got ' + element.length + ' elements.'
+    }
+    return [element, des]
+}
+
+console.log(countAndDescribe("Hello"))
+console.log(countAndDescribe(["Hello", "World"]))
+
+// The keyof Constraints
+
+function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) {
+    return 'Value : ' + obj[key];
+}
+
+
+console.log(extractAndConvert({name: 'Max'}, 'name'))
+
