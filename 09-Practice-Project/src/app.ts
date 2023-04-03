01780d2cce171898e85b0ad957826033ae50496d
Darshan Limbani <darshanlimbani94@gmail.com>
2023-04-03 13:26:54 +0530

    Finishing Drag & Drop. (Completed Project)

--- 09-Practice-Project/src/app.ts
+++ 09-Practice-Project/src/app.ts
@@ -75,6 +75,21 @@ class ProjectState extends State<Project> {
 
         this.projects.push(newProject);
 
+        this.updateListeners()
+    }
+
+    moveProject(projectId:string,newStatus:ProjectStatus){
+
+        const project = this.projects.find(prj=>prj.id === projectId);
+        if(project && project.status!== newStatus){
+            project.status = newStatus;
+            this.updateListeners()
+        }
+
+
+    }
+
+    private updateListeners(){
         for (const listenerFn of this.listeners) {
             listenerFn(this.projects.slice())
         }
@@ -244,9 +259,12 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> implements Drag
         }
     }
 
+    @autoBind
     dropHandler(event: DragEvent) {
         console.log('dropHandler......')
-        console.log(event.dataTransfer!.getData('text/plain'))
+        const prjId = event.dataTransfer!.getData('text/plain');
+
+        projectState.moveProject(prjId,this.type==='active'?ProjectStatus.Active:ProjectStatus.Finished)
     }
 
     @autoBind
